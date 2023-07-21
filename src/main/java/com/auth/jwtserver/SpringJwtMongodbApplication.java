f9658c2ce3488f1b0a2774bd4b6d7414d45a0499
jesusfb <jfigueroa.beltran@gmail.com>
2023-07-20 22:13:07 -0700

    Update SpringJwtMongodbApplication.java

--- src/main/java/com/auth/jwtserver/SpringJwtMongodbApplication.java
+++ src/main/java/com/auth/jwtserver/SpringJwtMongodbApplication.java
@@ -17,8 +17,8 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @SpringBootApplication
 @EnableMongoRepositories
 @OpenAPIDefinition(
-	info = @Info(title = "Spring Boot JWT Authentication Server API", version="1.0"), 
-	servers = { @Server(description = "Local Env", url="http://localhost:8080")
+	info = @Info(title = "Spring Boot JWT Authentication Server API", version="1.0")
+	
 })
 @SecurityScheme(
 	name = "bearerAuthToken",
